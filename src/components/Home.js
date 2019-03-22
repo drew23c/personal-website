edd4a94d37704434d283bab69fde8e6321a3d755
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-03-22 14:16:28 -0400

    updated the update section on the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,12 @@ export default class Home extends Component{
         this.updates = [
             {
                 date:"03/19/2019",
-                update:`I accepted a position as a Production Control Technician on 
+                update:`Accepted a position as a Production Control Technician on 
                 the Database team at 1199SEIU.`
+            },
+            {
+                date:"03/19/2019",
+                update:`Commissioned to help Grace Moravian Church redesign their website.`
             }
         ]
     }
