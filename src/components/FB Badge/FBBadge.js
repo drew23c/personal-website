874930e08f2b68e25f6552472283bccb6743029d
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-05-28 22:34:34 -0400

    added a different facebook badge

--- src/components/FB Badge/FBBadge.js
+++ src/components/FB Badge/FBBadge.js
@@ -6,7 +6,7 @@ export default class FBBadge extends Component{
     render(){
         return(
             <div className="fb-badge">
-                <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fbluecanvascreative%2F&width=450&layout=standard&action=like&size=large&show_faces=true&share=true&height=80&appId" width="450" height="80" style={{border:'none',overflow:'hidden'}} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbluecanvascreative%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style={{border:'none',overflow:'hidden'}} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
             </div>
         )
     }
